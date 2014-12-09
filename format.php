903d57c80b99e4c22952d080fe6388a4d4589147
Bruno Borges Paschoalinoto <bruno@paschoalinoto.com>
2014-12-09 11:57:38 -0200

    Documentation

--- format.php
+++ format.php
@@ -1,5 +1,16 @@
 <?php
 
+/* === Format.php ===
+   
+   This is the file that is included to format
+   things such as thread links and greentext
+   to show it to the user. It must be included
+   via include("format.php") in order to be used.
+
+   Author: Bruno02468
+   
+ */
+
 function format($str) {
     $threadlinks = "/(==(\d*)(<br>)*)/";
     $threadrepls = "<a href='http://bruno02468.com/4text/bread/$2'>==$2</a>$3";
