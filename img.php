65510eef92c729b31c43f8a7b7f63b74689b237b
Bruno Borges Paschoalinoto <bruno@paschoalinoto.com>
2014-12-09 12:30:34 -0200

    Documentation

--- img.php
+++ img.php
@@ -1,6 +1,14 @@
 <?php
 
+/* ===img.php===
+   
+   This page returns the HTML necessary to
+   show the banners linking to whatever
+   Kevin wants them to link.
 
+   Author: Bruno02468
+
+ */
 
 $f_contents = explode("\r\n", file_get_contents("http://dl.dropboxusercontent.com/u/141478576/SpookyBoard/SpookyBoard.txt")); 
 $index = rand(0, count($f_contents) - 1);
