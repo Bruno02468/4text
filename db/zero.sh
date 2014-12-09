29cdb7786361d2d0c50b29f8655b33cb40b24893
Bruno Borges Paschoalinoto <bruno@paschoalinoto.com>
2014-12-09 12:36:28 -0200

    Documentation

--- db/zero.sh
+++ db/zero.sh
@@ -1,4 +1,11 @@
-#/bin/sh
+#!/bin/sh
+# Deletes all threads and resets the counter.
+# Please execute upon making changes to add_thread.php and/or
+# add_answer.php. Execution assuming you're in the main folder:
+#
+#  $ cd db; ./zero.sh
+#
+# Author: Bruno02468
 
 rm threads/*
 echo 0 > counter
